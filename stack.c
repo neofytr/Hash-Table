dd446f0795ea25d1b25d3855e72798ac1abf5ebf
neofytr <imagination12344321.com@gmail.com>
2025-03-17 15:55:07 +0530

    stack implementation complete

---
+++ stack.c
@@ -0,0 +1,78 @@
+#include "stack.h"
+
+#include <string.h>
+
+stack_t *stack_create(size_t data_size)
+{
+    stack_t *stack = (stack_t *)malloc(sizeof(stack_t));
+    if (!stack)
+    {
+        return NULL;
+    }
+
+    stack->data_size = data_size;
+    stack->bottom_node = NULL;
+
+    return stack;
+}
+
+bool is_stack_empty(stack_t *stack)
+{
+    return !stack->bottom_node;
+}
+
+bool stack_push(stack_t *stack, void *data)
+{
+    if (!stack || !data)
+    {
+        return false;
+    }
+
+    stack_node_t *node = (stack_node_t *)malloc(sizeof(stack_node_t));
+    if (!node)
+    {
+        return false;
+    }
+
+    node->data = malloc(stack->data_size);
+    if (!node->data)
+    {
+        free(node);
+        return false;
+    }
+
+    // copy the data into the new node
+    if (!memcpy(node->data, data, stack->data_size))
+    {
+        free(node->data);
+        free(node);
+        return false;
+    }
+
+    // insert the new node at the stack bottom
+    node->next = stack->bottom_node;
+    stack->bottom_node = node;
+
+    return true;
+}
+
+bool stack_pop(stack_t *stack, void *data)
+{
+    if (!stack || !data || is_stack_empty(stack))
+    {
+        return false;
+    }
+
+    stack_node_t *node = stack->bottom_node;
+    stack->bottom_node = node->next;
+
+    if (!memcpy(data, node->data, stack->data_size))
+    {
+        return false;
+    }
+
+    free(node->data);
+    free(node);
+
+    return true;
+}
--- stack.h
+++ stack.h
@@ -1,13 +1,26 @@
 #ifndef STACK_H
 #define STACK_H
 
+#include <stdlib.h>
+#include <stdbool.h>
+
 typedef struct stack_node_ stack_node_t;
-typedef stack_node_t stack_t;
 
 struct stack_node_
 {
     stack_node_t *next;
     void *data;
 };
+
+typedef struct
+{
+    stack_node_t *bottom_node; // insertion and deletion happens at the bottom node only
+    size_t data_size;
+} stack_t;
+
+stack_t *stack_create(size_t data_size); // data size in bytes
+bool stack_push(stack_t *stack, void *data);
+bool is_stack_empty(stack_t *stack);
+bool stack_pop(stack_t *stack, void *data); // the popped data is stored in data
 
 #endif
